c745151cc9ce352b62391ea1b584c32fbfcae919
haider-codes23@github.com <haider.codes23@gmail.com>
2022-07-27 11:52:30 +0500

    Fixing Game File

--- lesson_6/21_game_bonus_features.js
+++ lesson_6/21_game_bonus_features.js
@@ -1,21 +1,22 @@
 const READLINE = require("readline-sync");
 require("cli-color");
 require("colors");
-const MAX_ACE_VALUE = 11;
+// const MAX_ACE_VALUE = 11;
 const MAX_SUM = 21;
-const DEALER_STAY_VALUE = 17;
+const MAX_WINS = 5;
+const DEALER_CUTOFF_VALUE = 17;
 const KING_QUEEN_JACK_VALUE = 10;
+const ACE_VALUE = 11;
 const ACE_CORRECTION = 10;
-const MAX_ROUNDS = 5;
-const suits = {H: "\u2665", D: "\u2666", C: "\u2663", S: "\u2660"};
+const DEAL_ONE_CARD = 1;
+const suits = {Heart: "\u2665", Diamond: "\u2666", Club: "\u2663", Spade: "\u2660"};
 const values = {
   two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9,
   ten: 10, K: 'King', Q: "Queen", J: "Jack", A: "Ace",
 };
 
-function displayWelcomeMessage() {
-  prompt(`Welcome to 21-Game`.bgYellow.bold);
-  prompt("It's a Fantastic Game, Hope you have Fun playing it!".bgYellow.bold);
+function prompt(message) {
+  console.log(`=> ${message}`);
 }
 
 function displayGameRules() {
@@ -27,15 +28,12 @@ function displayGameRules() {
   prompt("If the total Value of the Cards in Hand exceeds 21 then Ace will be worth 1 otherWise 11".bgGreen.bold);
   prompt("Both Player and dealer have to make sure their Card's Total value doesn't exceed 21 otherwise they'd get busted".bgGreen.bold);
   prompt("A round is considered only when Player or Dealer Wins/loses".bgRed.bold);
-  prompt("Whoever wins the most number of Rounds wins the overall Match".bgRed.bold);
 }
 
 function displayPlayerRules() {
   prompt("PLAYER RULES".blue.bold);
   prompt('Player can either choose to Hit or Stay depending on the value of Cards in Hand and what the Player thinks the Dealer has!'.bgBrightBlue.bold);
   prompt('The Player can continue to hit as many Times as they want unless Player gets busted'.bgBrightBlue.bold);
-  prompt("For player to get busted while its his turn, his total value of cards in hand must be greater than 21".bgBrightBlue.bold);
-  prompt("Stay means Player doesn't need to be dealt another card".bgBrightBlue.bold);
 }
 
 function displayDealerRules() {
@@ -44,84 +42,155 @@ function displayDealerRules() {
   prompt("Dealer Keeps on Hitting until the Total Value of Cards in Hand Reaches 17".bgCyan.bold);
 }
 
-function prompt(message) {
-  console.log(`=> ${message}`);
+function displaySymbolNames() {
+  prompt(`${suits.Heart}`.red + ` => ` + `${Object.keys(suits)[0]}`.red.bold);
+  prompt(`${suits.Diamond}`.white + ` => ` + `${Object.keys(suits)[1]}`.white.bold);
+  prompt(`${suits.Club}`.yellow + ` => ` + `${Object.keys(suits)[2]}`.yellow.bold);
+  prompt(`${suits.Heart}`.green + ` => ` + `${Object.keys(suits)[3]}`.green);
+  console.log(" ");
+
 }
 
 function shuffle(array) {
   for (let first = array.length - 1; first > 0; first--) {
-    let second = Math.floor(Math.random() * (first + 1));// not clear
+    let second = Math.floor(Math.random() * (first + 1));
     [array[first], array[second]] = [array[second], array[first]];
   }
 
+
   return array;
 }
-// Every Card has a suit and a value/rank
-// we need to create a deck of 52 cards, each having a suit and a rank
-// The Data- structure storing the suits and value/rank are Objects
-// Each suit(e.g. HEARTS) has cards ranging from value/rank = 2 through
-// value/rank = 10 ,JACK, KING, QUEEN and ACE
-// The Data-Structure we'll be using to create a card will be an object
-// The Data- structure that'll be holding the cards/ objects will be an array
-// we'll take one suit at a time and one rank/value
-// Next we going to
 
 function initializeDeck() {
   let deck = [];
 
-  suits.forEach(suit => {
-    values.forEach(value => {
+  Object.values(suits).forEach(suit => {
+    Object.values(values).forEach(value => {
       deck.push({suit, value});
     });
   });
 
-
-  return deck;
+  return shuffle(deck);
 }
 
-function popTwoCards(deck) {
-  return [deck.pop(), deck.pop()];
+function dealCards(cards, numberOfCards) {
+  let cardsDealt = [];
+  for (let card = 0; card < numberOfCards; card++) {
+    cardsDealt.push(cards.pop());
+  }
+
+  return cardsDealt;
 }
 
-function hand(cards) {
-  return cards.map(card => card.join(' of '));
+function dealNewCard(cards, deck) {
+  cards.push(dealCards(deck, DEAL_ONE_CARD)[0]);
+  return cards;
 }
 
-function total(cards) {
+function calculateTotal(cards) {
   let sum = 0;
   cards.forEach(card => {
-    if (card[0] === "Ace") {
-      sum += MAX_ACE_VALUE;
-    } else if (['King', 'Queen', 'Jack'].includes(card[0])) {
+    if (card.value === 'Ace')  {
+      sum += ACE_VALUE;
+    } else if (['King', 'Queen', 'Jack'].includes(card.value)) {
       sum += KING_QUEEN_JACK_VALUE;
     } else {
-      sum += card[0];
+      sum += card.value;
     }
   });
 
-  // Ace correction
-  cards.filter(card => card[0] === "Ace").forEach(_ => {
-    if (sum > MAX_SUM) {
-      sum -= ACE_CORRECTION;
+  // Ace Correction
+  cards.filter(card => card.value === 'Ace').forEach(_ => {
+    if (sum > 21) {
+      sum -= ACE_CORRECTION; // This way if we have a Ace in our hand and our total exceeds 21 then we subtract 10
+      // from the total, that way Ace will have a value of 1 insted of 11.
     }
   });
 
   return sum;
 }
 
-function busted(handValue) {
-  return handValue > MAX_SUM;
+function getCardName(card) {
+  //{ suit:  , value: }
+  return `[ ${card.value} of ${card.suit} ]`;
+
 }
 
-function detectResult (playersHandValue, dealersHandValue) {
+function busted(cardsTotal) {
+  return cardsTotal > MAX_SUM;
+}
 
-  if (playersHandValue > MAX_SUM) {
+function displayInitialCards(playerCards, dealerCards,playerTotal) {
+  console.log(" ");
+  prompt(`YOU HAVE ${getCardName(playerCards[0]) } AND ${getCardName(playerCards[1]) }, HAVING A TOTAL VALUE OF: ${playerTotal}`.green.bold);
+  prompt(`DEALER HAS  ${getCardName(dealerCards[0])} AND ONE UNKNOWNCARD`.blue.bold);
+
+}
+
+
+function promptStartMatch() {
+  let choice = READLINE.question(prompt(`PRESS 's' TO START A MATCH WHERE THE WINNER IS ONE WHO FIRST WINS 5 ROUNDS OR PRESS 'e' TO EXIT GAME`.red.bold));
+  while (!['s', 'e'].includes(choice)) {
+    prompt("Sorry wrong Choice, Please choose Again".bgRed.bold);
+    prompt(`Press 's' to Start a Match consisting of 5 Rounds or press 'e' to exit the Game`.bgRed.bold);
+    choice = READLINE.question();
+  }
+  return choice;
+}
+
+function promptHitOrStay() {
+
+  prompt("Would you like to (h)it or (S)tay".red.bold);
+  let playerChoice = READLINE.question();
+  while (!['h', 's'].includes(playerChoice)) {
+    prompt("sorry, Invlaid Choice must enter 'h' or 's'".bgRed.bold);
+    playerChoice = READLINE.question();
+  }
+
+  //console.clear();
+  return playerChoice;
+}
+
+function showAllCards(playerCards, dealerCards, playerTotal, dealerTotal) {
+  let playerCardsString = '';
+  let dealerCardsString = '';
+  playerCards.forEach(playerCard => {
+    playerCardsString += getCardName(playerCard);
+  });
+  prompt(" ");
+  prompt(`YOU HAVE ${playerCardsString}, HAVING A TOTAL OF ${playerTotal}`.green.bold);
+
+  dealerCards.forEach(card => {
+    dealerCardsString += getCardName(card);
+    //prompt(`${getCardName(card)}`);
+  });
+
+  prompt(`DEALER HAS ${dealerCardsString}, HAVING A TOTAL OF ${dealerTotal}`.blue.bold);
+
+
+}
+
+function showDealerCards(dealerCards, dealerTotal) {
+  let dealerCardsString = '';
+  dealerCards.forEach(card => {
+    dealerCardsString += getCardName(card);
+    //prompt(`${getCardName(card)}`);
+  });
+  prompt(`DEALER HAS ${dealerCardsString}, HAVING A TOTAL OF ${dealerTotal}`.blue.bold);
+
+
+}
+
+
+function detectResult (playerTotal, dealerTotal) {
+
+  if (playerTotal > MAX_SUM) {
     return 'PLAYER_BUSTED';
-  } else if (dealersHandValue > MAX_SUM) {
+  } else if (dealerTotal > MAX_SUM) {
     return 'DEALER_BUSTED';
-  } else if (playersHandValue > dealersHandValue) {
+  } else if (playerTotal > dealerTotal) {
     return 'PLAYER_WON';
-  } else if (playersHandValue < dealersHandValue) {
+  } else if (playerTotal < dealerTotal) {
     return 'DEALER_WON';
   } else {
     return 'TIE';
@@ -129,15 +198,15 @@ function detectResult (playersHandValue, dealersHandValue) {
 
 }
 
-function displayResult (playersHandValue, dealersHandValue) {
-  let result = detectResult(playersHandValue, dealersHandValue);
+function displayResult (playerTotal, dealerTotal) {
+  let result = detectResult(playerTotal, dealerTotal);
 
   switch (result) {
     case "PLAYER_BUSTED":
-      prompt("YOUR BUSTED, DEALER WON".bgMagenta.bold);
+      prompt("YOUR BUSTED, DEALER WON THE ROUND".bgMagenta.bold);
       break;
     case "DEALER_BUSTED":
-      prompt("DEALER BUSTED, PLAYER WON".bgMagenta.bold);
+      prompt("DEALER BUSTED, PLAYER WON THE ROUND".bgMagenta.bold);
       break;
     case "PLAYER_WON":
       prompt("YOU WON, DEALER BUSTED".bgMagenta.bold);
@@ -150,16 +219,57 @@ function displayResult (playersHandValue, dealersHandValue) {
   }
 }
 
-// eslint-disable-next-line max-len
-function roundOutput (playerCards, dealerCards, playersHandValue, dealersHandValue) {
-  prompt(`You have ${hand(playerCards)}, having a total of ${playersHandValue}`.green.bold);
-  prompt(`Dealer has ${hand(dealerCards)}, having a total of ${dealersHandValue}`.green.bold);
+
+function showDealtCards(playerCards, dealerCards, playerTotal) {
+  let playerCardsStringLiteral = '';
+  playerCards.forEach(card => {
+    playerCardsStringLiteral += ' ';
+    playerCardsStringLiteral += getCardName(card);
+
+  });
+  prompt(`YOU HAVE ${playerCardsStringLiteral}, HAVING A TOTAL OF ${playerTotal} `.green.bold);
+  prompt(`DEALER HAS ${getCardName(dealerCards[0])} AND ONE UNKNOWN-CARD`.blue.bold);
+
+}
+
+
+function updateScore(playerTotal, dealerTotal, scores) {
+  if (detectResult(playerTotal, dealerTotal) === "DEALER_WON" ||
+    detectResult(playerTotal, dealerTotal) === "PLAYER_BUSTED") {
+    scores.dealerWins += 1;
+  } else if (detectResult(playerTotal, dealerTotal) === "DEALER_BUSTED" ||
+    detectResult(playerTotal, dealerTotal) === "PLAYER_WON") {
+    scores.playerWins += 1;
+  }
+}
+
+function updateRound(playerTotal, dealerTotal, scores) {
+  if (detectResult(playerTotal, dealerTotal) !== "TIE") {
+    scores.round += 1;
+  }
 }
 
 function displayScores(scores) {
+  console.log(" ");
   prompt(`Rounds won by player: ${scores.playerWins}`.green.bold);
   prompt(`Rounds won by dealer: ${scores.dealerWins}`.green.bold);
-  prompt(`Number of rounds completed: ${scores.roundsPlayed}`.green.bold);
+  prompt(`#Round: ${scores.round}`.green.bold);
+}
+
+function matchWon(scores) {
+  if (scores.playerWins === MAX_WINS || scores.dealerWins === MAX_WINS) {
+    return true;
+  } else {
+    return 0;
+  }
+}
+
+function displayMatchWinner (scores) {
+  if (scores.playerWins === MAX_WINS) {
+    prompt("PLAYER WON THE MATCH".bgGreen.bold);
+  } else if (scores.dealerWins === MAX_WINS) {
+    prompt("DEALER WON THE MATCH".bgGreen.bold);
+  }
 }
 
 function nextRound() {
@@ -168,148 +278,102 @@ function nextRound() {
   console.clear();
 
 }
-
-function updateScore(playersHandValue, dealersHandValue, scores) {
-  if (detectResult(playersHandValue, dealersHandValue) === "DEALER_WON" ||
-    detectResult(playersHandValue, dealersHandValue) === "PLAYER_BUSTED") {
-    scores.dealerWins += 1;
-    scores.roundsPlayed += 1;
-  } else if (detectResult(playersHandValue, dealersHandValue) === "DEALER_BUSTED" ||
-    detectResult(playersHandValue, dealersHandValue) === "PLAYER_WON") {
-    scores.playerWins += 1;
-    scores.roundsPlayed += 1;
-  }
-}
-
-function playerTurn ( playerCards, playersHandValue, deck ) {
-  let playerChoice;
-  do {
-    prompt("Would you like to (h)it or (S)tay".green.bold);
-    playerChoice = READLINE.question();
-    while (!['h', 's'].includes(playerChoice)) {
-      prompt("sorry, Invlaid Choice must enter 'h' or 's'".bgRed.bold);
-      playerChoice = READLINE.question();
-    }
-    console.clear();
-
-    if (playerChoice === 'h') {
-      playerCards.push(deck.pop());
-      playersHandValue = total(playerCards);
-      prompt("YOU CHOOSE TO HIT".green.bold);
-      prompt(`Cards in your hands are ${hand(playerCards).join(", ")}, having a total value of ${playersHandValue}`.green.bold);
-    }
-  } while (playerChoice !== 's' && !busted(playersHandValue));
-
-  return [playerChoice, playersHandValue, playerCards];
-}
-
-function displayMatchWinner (scores) {
-  if (scores.playerWins > scores.dealerWins) {
-    prompt("PLAYER WON THE MATCH".bgGreen.bold);
-  } else {
-    prompt("DEALER WON THE MATCH".bgGreen.bold);
-  }
-}
-
-displayWelcomeMessage();
-READLINE.question(prompt("Hit Enter Key to Proceed to the Game".red.bold));
 console.clear();
-// Outer Game loop
+displayGameRules();
+displayPlayerRules();
+displayDealerRules();
+
 while (true) {
-  displayGameRules();
-  let scores = { playerWins: 0, dealerWins: 0, roundsPlayed: 0 };
+  let scores = { playerWins: 0, dealerWins: 0, round: 1 };
+  let startMatch = promptStartMatch();
+  if (startMatch === 'e') break;
+  console.clear();
 
-  let choice = READLINE.question(prompt(`Press 's' to Start a Match consisting of 5 Rounds or press 'e' to exit the Game`.red.bold));
-  while (!['s', 'e'].includes(choice)) {
-    prompt("Sorry wrong Choice, Please choose Again".bgRed.bold);
-    prompt(`Press 's' to Start a Match consisting of 5 Rounds or press 'e' to exit the Game`.bgRed.bold);
-    choice = READLINE.question();
-  }
-  if (choice === 'e') break;
-
-  while (scores.roundsPlayed < MAX_ROUNDS) {
-    console.clear();
+  while (true) {
+    //console.clear();
     let deck = initializeDeck();
+    let playerChoice;
 
     let playerCards = [];
     let dealerCards = [];
 
-    playerCards.push(...popTwoCards(deck));
-    dealerCards.push(...popTwoCards(deck));
+    playerCards = dealCards(deck, 2);
+    dealerCards = dealCards(deck, 2);
 
-    let playersHandValue = total(playerCards);
-    let dealersHandValue = total(dealerCards);
+    let playerTotal = calculateTotal(playerCards);
+    let dealerTotal = calculateTotal(dealerCards);
 
+    displaySymbolNames();
     displayScores(scores);
+    displayInitialCards(playerCards, dealerCards, playerTotal);
 
-    prompt(`You have ${hand(playerCards).join(' and ')}, having a total value of ${playersHandValue}`.green.bold);
-    prompt(`Dealer has ${dealerCards[0].join(' of ')}`.green.bold);
-    // Players Turn
-    // Player Makes a Choice to either Hit or Stay
-    let playerChoice;
+    //player turn
+    while (true) {
 
-    displayDealerRules();
-    displayPlayerRules();
-    // eslint-disable-next-line max-len
-    [playerChoice, playersHandValue, playerCards] = playerTurn(playerCards, playersHandValue, deck);
-    
-    
-    if (busted(playersHandValue)) {
-      roundOutput(playerCards, dealerCards, playersHandValue, dealersHandValue);
-      displayResult(playersHandValue, dealersHandValue);
-      updateScore(playersHandValue, dealersHandValue, scores);
+      playerChoice = promptHitOrStay();
+      console.clear();
+      if (playerChoice === 's') break;
+
+      displaySymbolNames();
+      dealNewCard(playerCards, deck);
+      playerTotal = calculateTotal(playerCards);
+
+      prompt("YOU CHOOSE TO HIT".green.bold);
+      showDealtCards(playerCards, dealerCards, playerTotal);
       displayScores(scores);
-      if (scores.roundsPlayed === MAX_ROUNDS) {
-        prompt("The Match is over".bgGreen.bold);
+
+      if (busted(playerTotal)) break;
+    }
+
+    console.clear();
+    if (busted(playerTotal)) {
+      showAllCards(playerCards, dealerCards, playerTotal, dealerTotal);
+
+      updateScore(playerTotal, dealerTotal, scores);
+      displayResult(playerTotal, dealerTotal);
+      displayScores(scores);
+
+      if (matchWon(scores)) {
         displayMatchWinner(scores);
+        break;
       } else {
+        updateRound(playerTotal, dealerTotal, scores);
         nextRound();
         continue;
       }
-
     }
-
+    prompt(`YOU CHOOSE TO STAY, HAVING A TOTAL OF ${playerTotal}`.green.bold);
     if (playerChoice === 's') {
       // Dealers Turn
-      while (dealersHandValue <= DEALER_STAY_VALUE) {
-        prompt("Dealer Hits".green.bold);
-        dealerCards.push(deck.pop());
-        dealersHandValue = total(dealerCards);
-        prompt(`Dealer has ${hand(dealerCards).join(', ')}, having a total value of ${dealersHandValue}`.bgGreen.bold);
-      }
+      while (dealerTotal <= DEALER_CUTOFF_VALUE) {
+        prompt("DEALER HITS".blue.bold);
+        dealNewCard(dealerCards, deck);
 
-      if (busted(dealersHandValue)) {
-        // eslint-disable-next-line max-len
-        roundOutput(playerCards, dealerCards, playersHandValue, dealersHandValue);
-        displayResult(playersHandValue, dealersHandValue);
-        updateScore(playersHandValue, dealersHandValue, scores);
-        displayScores(scores);
-        // eslint-disable-next-line max-depth
-        if (scores.roundsPlayed === MAX_ROUNDS) {
-          prompt("The Match is over".bgGreen.bold);
-          displayMatchWinner(scores);
-        }
-        nextRound();
-        continue;
+        dealerTotal = calculateTotal(dealerCards);
+        showDealerCards(dealerCards, dealerTotal);
 
-
-      } else {
-        prompt("Dealer Choose to stay".green.bold);
-      }
-
-      roundOutput(playerCards, dealerCards, playersHandValue, dealersHandValue);
-      displayResult(playersHandValue, dealersHandValue);
-      updateScore(playersHandValue, dealersHandValue, scores);
-      displayScores(scores);
-      if (scores.roundsPlayed === MAX_ROUNDS) {
-        prompt("THE MATCH IS OVER".bgGreen.bold);
-        displayMatchWinner(scores);
-      } else {
-        nextRound();
-        continue;
       }
     }
+    showAllCards(playerCards, dealerCards, playerTotal, dealerTotal);
+
+    if (dealerTotal >= DEALER_CUTOFF_VALUE && dealerTotal <= MAX_SUM) {
+      prompt(`DEALER CHOOSE TO STAY, AT ${dealerTotal}`.blue.bold);
+    }
+    displayResult(playerTotal, dealerTotal);
+    updateScore(playerTotal, dealerTotal, scores);
+    displayScores(scores);
+
+    if (matchWon(scores)) {
+      displayMatchWinner(scores);
+      break;
+    } else {
+      updateRound(playerTotal, dealerTotal, scores);
+      nextRound();
+      continue;
+    }
+
 
   }
+
 
 }
